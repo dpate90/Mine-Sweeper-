7aad8c93e4f75acdbce58f8eaf791fa8913dc17d
wssddtc <swang224@uic.edu>
2016-02-14 03:22:40 -0600

    Create mine_sweeper.java
    main function

---
+++ mine_sweeper.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class mine_sweeper {
+	// main function
+		public static void main(String[] args) throws IOException {
+
+			// game loop
+			while (true) {
+
+				Game game = new Game(); 				
+
+				//if () {
+				//	System.exit(0);
+				//}
+			}
+		}
+
+}
